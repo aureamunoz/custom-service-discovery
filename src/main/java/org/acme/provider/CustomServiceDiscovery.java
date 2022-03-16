254cd20926d37dd64c554c881204226c0ef356c2
Auri Munoz <amunozhe@redhat.com>
2022-03-16 12:51:16 +0100

    update stork version and code accordingly

--- src/main/java/org/acme/provider/CustomServiceDiscovery.java
+++ src/main/java/org/acme/provider/CustomServiceDiscovery.java
@@ -1,14 +1,13 @@
 package org.acme.provider;
 
 import io.smallrye.mutiny.Uni;
-import io.smallrye.stork.DefaultServiceInstance;
-import io.smallrye.stork.ServiceDiscovery;
-import io.smallrye.stork.ServiceInstance;
-import io.smallrye.stork.spi.ServiceInstanceIds;
+import io.smallrye.stork.api.ServiceDiscovery;
+import io.smallrye.stork.api.ServiceInstance;
+import io.smallrye.stork.impl.DefaultServiceInstance;
+import io.smallrye.stork.utils.ServiceInstanceIds;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class CustomServiceDiscovery implements ServiceDiscovery {
 
--- src/main/java/org/acme/provider/CustomServiceDiscoveryProvider.java
+++ src/main/java/org/acme/provider/CustomServiceDiscoveryProvider.java
@@ -1,20 +1,20 @@
 package org.acme.provider;
 
-import io.smallrye.stork.ServiceDiscovery;
-import io.smallrye.stork.config.ServiceConfig;
-import io.smallrye.stork.config.ServiceDiscoveryConfig;
+import io.smallrye.stork.api.ServiceDiscovery;
+import io.smallrye.stork.api.config.ServiceConfig;
+import io.smallrye.stork.api.config.ServiceDiscoveryAttribute;
+import io.smallrye.stork.api.config.ServiceDiscoveryType;
+import io.smallrye.stork.spi.StorkInfrastructure;
 import io.smallrye.stork.spi.ServiceDiscoveryProvider;
 
-public class CustomServiceDiscoveryProvider implements ServiceDiscoveryProvider {
-    @Override
-    public String type() {
-        return "custom";
-    }
+@ServiceDiscoveryAttribute(name = "custom-host", description = "The Consul host.", defaultValue = "localhost")
+@ServiceDiscoveryAttribute(name = "custom-port", description = "The Consul port.", defaultValue = "8500")
+@ServiceDiscoveryAttribute(name = "refresh-period", description = "Service discovery cache refresh period.", defaultValue = "5M")
+@ServiceDiscoveryType("custom")
+public class CustomServiceDiscoveryProvider implements ServiceDiscoveryProvider<CustomServiceDiscoveryProviderConfiguration> {
 
     @Override
-    public ServiceDiscovery createServiceDiscovery(ServiceDiscoveryConfig config,
-                                                   String serviceName,
-                                                   ServiceConfig serviceConfig) {
-        return new CustomServiceDiscovery();
+    public ServiceDiscovery createServiceDiscovery(CustomServiceDiscoveryProviderConfiguration customServiceDiscoveryProviderConfiguration, String s, ServiceConfig serviceConfig, StorkInfrastructure storkInfrastructure) {
+        return null;
     }
 }
